d6b273c13ac33b6639f475beae2484e53642209a
hugosenari <hugosenari@gmail.com>
2022-03-12 03:30:03 -0300

    chore: re index termbox

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Termbox wrapper.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-termbox-master.flake = false;
+  inputs.src-termbox-master.owner = "fowlmouth";
+  inputs.src-termbox-master.ref   = "refs/heads/master";
+  inputs.src-termbox-master.repo  = "nim-termbox";
+  inputs.src-termbox-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-termbox-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-termbox-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
