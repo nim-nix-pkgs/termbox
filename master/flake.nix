e75e2e195cd750d77fc26694949abad44e6f4264
hugosenari <hugosenari@gmail.com>
2022-03-23 22:53:38 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Termbox wrapper.'';
+  description = ''Wrapper for Termbox, it beats the hell out of curses. (https://github.com/nsf/termbox)'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
